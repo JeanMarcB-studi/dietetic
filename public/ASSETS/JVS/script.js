e61ea0fd3ce700aba0faf0d660fc5c8d424df697
Jean-Marc BOUTAUD <jmbstudi@gmail.com>
2023-06-21 14:32:15 +0200

    The fetch/controller has got some cleaning and is still working the same.

--- public/ASSETS/JVS/script.js
+++ public/ASSETS/JVS/script.js
@@ -22,43 +22,32 @@
 // }
 console.log("start engine")
 
-// /note/{idRecette}{note}{message}'
-
-// const url="http://localhost/PHP/symfo_diete/note/"
-// const url="https://127.0.0.1:8000/note2/"
-const url="https://127.0.0.1:8000/tata/"
-
+const url="https://127.0.0.1:8000/note/"
 const bton = document.querySelector("#send")
 
 bton.addEventListener('click', (e) => {
   console.log('click')
   // e.target.classList.add('btonQuarterSelected')
-  console.log('URL: '+url)
-  
-  
-  // ----------------------------------------- FETCH POST QUI FONCTIONNE ? --------
-  
-  let hh = new Headers();
   
   let data = JSON.stringify({
-    id: '117',
-    name:"yy"
-  //   idRec: '1',
-  //   note: '3',
-  //  message: 'coucou',
+    id: 'oss117',
+    name:"yyY"
   })
-
-//POST
 
 var request = new Request(url, {
   method: 'POST',
   body: data,
-  headers: hh
+  headers: new Headers()
 });
 
 fetch(request)
 .then(response => response.json())
-.then(response => console.log(JSON.stringify(response)))
-.catch(error => console.log("Erreur : " + error));
 
+// RETURN DATA IS OK
+.then(response => {
+  console.log(response.id + " " + response.name)
+})
+
+//IF ERROR
+.catch(error => console.log("Erreur : " + error));
 })
--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -11,81 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 // use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;// marche que à partir symf 6.3
 
 
-class tataController
-{
-    #[Route('/tata/', name: 'app_tata')]
-    public function number(request $request): Response
-    {
-        $req = $request->query;
-        $id = $req->get('id');
-        $name = $req->get('name');
-
-        $data = json_decode($request->getContent(), true);
-        
-        $id = $data['id'];
-        $name = $data['name'];
-        // dump($id);
-
-        return new Response(
-            "<html><body>id $id name $name</body></html>"
-        );
-    }
-}
-
-class LuckyController
-{
-    #[Route('/toto/', name: 'app_lucky_number')]
-    public function number(request $request): JsonResponse
-    {
-        $req = $request->query;
-        dump($request);
-        $id1 = $req->get('id');
-        $id2 = $request->request->get('id');
-        // $key1 = $request->request->get('key1');
-        $name = $req->get('name');
-
-      // dd($req);
-
-        return new JsonResponse([
-            'id1' => $id1,
-            'id2' => $id2,
-            // 'id' => 'jjjj',
-            'name' => $req,
-            'toto' => 'yyY'
-        ]);
-    }
-    
-    // use Symfony\Component\HttpFoundation\Request;
-    // use Symfony\Component\HttpFoundation\Response;
-    
-
-    #[Route('/titi/', name: 'app_luck')]   
-    public function yourAction(Request $request)
-    {
-        $data = json_decode($request->getContent(), true);
-        
-        $id = $data['id'];
-        $name = $data['name'];
-        dump($id);
-        // ...
-
-    return new Response('Données reçues');
-}
-
-
-}
-
-
-
-
 class NoteController extends AbstractController
 {
-    #[Route('/note/{id}', name: 'app_note')]
-    public function showNote(Request $request): JsonResponse
-    {
-        $idRecette = $request->attributes->get('id');
-            return new JsonResponse([
-            'message' => 'This is the note with id '.$idRecette,
-        ]);
-    }
+  #[Route('/note/', name: 'app_note')]
+  public function number(request $request): JsonResponse
+  {
+
+      $data = json_decode($request->getContent(), true);
+      
+      $id = $data['id'];
+      $name = $data['name'];
+
+      return new JsonResponse([
+        'id' => $id,
+        'name' => $name,
+      ]);
+  }
 }
+
+
